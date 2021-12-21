98b1fec9a42ba28b220d168f736ec0be18d2bd71
Javid De Los Santos <javiddelossantos5@gmail.com>
2021-12-20 22:39:25 -0700

    bug fix

--- script.js
+++ script.js
@@ -20,9 +20,12 @@ function calculate() {
   const testWash14 = document.getElementById('testWash14').value * 14;
   const testWash16 = document.getElementById('testWash16').value * 16;
 
-  const receiptFromTill11 = document.getElementById('receiptFromTill11').value * 11;
-  const receiptFromTill14 = document.getElementById('receiptFromTill14').value * 14;
-  const receiptFromTill16 = document.getElementById('receiptFromTill16').value * 16;
+  const receiptFromTill11 =
+    document.getElementById('receiptFromTill11').value * 11;
+  const receiptFromTill14 =
+    document.getElementById('receiptFromTill14').value * 14;
+  const receiptFromTill16 =
+    document.getElementById('receiptFromTill16').value * 16;
 
   const reWash11 = document.getElementById('reWash11').value * 11;
   const reWash14 = document.getElementById('reWash14').value * 14;
@@ -36,31 +39,36 @@ function calculate() {
   const refund14 = document.getElementById('refund14').value * 14;
   const refund16 = document.getElementById('refund16').value * 16;
 
-
   testWashTotal = Number(testWash11) + Number(testWash14) + Number(testWash16);
-  receiptFromTillTotal = Number(receiptFromTill11) + Number(receiptFromTill14) + Number(receiptFromTill16);
+  receiptFromTillTotal =
+    Number(receiptFromTill11) +
+    Number(receiptFromTill14) +
+    Number(receiptFromTill16);
   reWashTotal = Number(reWash11) + Number(reWash14) + Number(reWash16);
   employeeTotal = Number(employee11) + Number(employee14) + Number(employee16);
   refundTotal = Number(refund11) + Number(refund14) + Number(refund16);
 
-  noCharge = Number(testWashTotal) + Number(receiptFromTillTotal) + Number(reWashTotal) + Number(employeeTotal) + Number(refundTotal);
+  noCharge =
+    Number(testWashTotal) +
+    Number(receiptFromTillTotal) +
+    Number(reWashTotal) +
+    Number(employeeTotal) +
+    Number(refundTotal);
 
-  
+  const oneValue = ones.value * 1;
+  const fivesValue = fives.value * 5;
+  const tensValue = tens.value * 10;
+  const twentiesValue = twenties.value * 20;
+  const fiftiesValue = fifties.value * 50;
+  const hundredValue = hundred.value * 100;
 
-  const oneValue = ones.value * 1
-  const fivesValue = fives.value * 5
-  const tensValue = tens.value * 10
-  const twentiesValue = twenties.value * 20
-  const fiftiesValue = fifties.value * 50
-  const hundredValue = hundred.value * 100
-
-
-
-    const cashTotal = Number(oneValue) + Number(fivesValue) + Number(tensValue) + Number(twentiesValue) + Number(fiftiesValue) + Number(hundredValue);
-
-
-
-    console.log(cashTotal);
+  const cashTotal =
+    Number(oneValue) +
+    Number(fivesValue) +
+    Number(tensValue) +
+    Number(twentiesValue) +
+    Number(fiftiesValue) +
+    Number(hundredValue);
 
   const totalOfTheDay =
     Number(cashTotal) +
@@ -74,12 +82,11 @@ function calculate() {
   const total = Number(washPilot) - Number(totalOfAddition);
   console.log(total);
 
-
   totalText.innerHTML = Number(total);
 
   if (total === 0) {
     difference.innerHTML = `Good Job ${workerName} Perfect 0`;
   } else {
     difference.innerHTML = `${workerName} the amount is off ${total} dollars`;
-  } 
+  }
 }
